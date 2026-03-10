d75941ebcd8205c041081371152e5ec462d2e3e8
Luis Benegas <luisbene12@gmail.com>
2026-03-10 20:40:27 -0300

    create pgpool

---
+++ src/infrastructure/database/connection.postgres.js
@@ -0,0 +1,9 @@
+import {Pool} from 'pg';
+
+export const pool = new Pool({
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'postgres',
+    password: process.env.DB_PASSWORD || '1314',
+    port: process.env.DB_PORT || 5432,
+});
