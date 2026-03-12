b4f32e6af84154054ccbbc415e703c0bd1cbea61
Luis Benegas <luisbene12@gmail.com>
2026-03-12 20:26:37 -0300

    se crea el insert

--- src/infrastructure/repositories/student.pg.repository.js
+++ src/infrastructure/repositories/student.pg.repository.js
@@ -3,6 +3,7 @@ import {pool} from '../database/connection.postgres.js';
 
 export class StudentPgRepository extends StudentRepository {
     create({id, name, email}){
-        
+        const result = pool.query('INSERT INTO students (id, name, email) VALUES ($1, $2, $3) RETURNING *', [id, name, email]);
+        return result.rows[0];
     }
 }
