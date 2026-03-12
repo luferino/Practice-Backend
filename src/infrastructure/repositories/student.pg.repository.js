a6956985a41cae07219add0e5e71f8e2026dccda
Luis Benegas <luisbene12@gmail.com>
2026-03-12 20:29:29 -0300

    se agrega entity al repository

--- src/infrastructure/repositories/student.pg.repository.js
+++ src/infrastructure/repositories/student.pg.repository.js
@@ -1,9 +1,15 @@
 import {StudentRepository} from '../../domine/repositories/student.repository.js';
 import {pool} from '../database/connection.postgres.js';
+import {Student} from '../../domine/entities/student.entity.js';
 
 export class StudentPgRepository extends StudentRepository {
     create({id, name, email}){
         const result = pool.query('INSERT INTO students (id, name, email) VALUES ($1, $2, $3) RETURNING *', [id, name, email]);
-        return result.rows[0];
+        return new Student(result.rows[0]);
+    }
+
+    findById(id){
+        const result = pool.query('SELECT * FROM students WHERE id = $1', [id]);
+        return new Student(result.rows[0]);
     }
 }
