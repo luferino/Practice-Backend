867c10c64d917902571b84a12d4202879ca99f3d
Luis Benegas <luisbene12@gmail.com>
2026-03-15 16:51:38 -0300

    controller

---
+++ src/modules/student/infrastructure/http/student.controller.js
@@ -0,0 +1,26 @@
+export class estudentController {
+    constructor(createStudentUseCase, listStudentUseCase){
+        this.createStudentUseCase = createStudentUseCase;
+        this.listStudentUseCase = listStudentUseCase;
+    }
+
+    create = async (req, res) => {
+        try {
+            const student = req.body;
+            const createdStudent = await this.createStudentUseCase.execute(student);
+            res.status(201).json(createdStudent);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+
+    list = async (req, res) => {
+        try {
+            const students = await this.listStudentUseCase.execute();
+            res.status(200).json(students);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+
+}
