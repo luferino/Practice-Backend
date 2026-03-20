7b48012bd9641ee793b7d858503e6fe5e166c484
Luis Benegas <luisbene12@gmail.com>
2026-03-19 22:04:14 -0300

    no me dio tiempo

--- src/app.js
+++ src/app.js
@@ -7,10 +7,11 @@ app.use(express.json());
 
 
 app.use('/students', studentRoutes())
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${PORT}`);
 });
