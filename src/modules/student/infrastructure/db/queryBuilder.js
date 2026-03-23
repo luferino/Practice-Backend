87eb5aa6c368565031d0ef657581f30fba459cf5
Luis Benegas <luisbene12@gmail.com>
2026-03-23 20:43:45 -0300

    se completa el query

--- src/modules/student/infrastructure/db/queryBuilder.js
+++ src/modules/student/infrastructure/db/queryBuilder.js
@@ -5,5 +5,20 @@ export const buildStudentQuery = ({filter = {}, like =  false}) => {
     const values = [];
 
     const allowedFields = ['id', 'name', 'email'];
+/*const safeFilters = Object.fromEntries(
+    Object.entries(filters).filter(([key]) =>
+        allowedFields.includes(key)
+    )
+);*/
+    const safeKeys = keys.filter(key => allowedFields.includes(key));
     
+    const conditions = safeKeys.map((key,i) => {
+        values.push(like ? `%${filter[key]}%` : filter[key]);
+        return `${key} ${like ? 'ILIKE' : '='} $${i + 1}`;
+    });
+    
+    return {
+        clause: conditions.join(' AND '),
+        values
+    };
 }
