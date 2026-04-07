708c5a9c9b71074ace93274dfc48aa5430b563b2
Luis Benegas <luisbene12@gmail.com>
2026-04-07 20:09:59 -0300

    router

---
+++ src/modules/subjects/infrastructure/http/subject.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { SubjectController } from "./subject.controller.js";
+import { CreateSubjectUseCase } from "../../application/create.subject.usecase.js";
+import { ListSubjectUseCase } from "../../application/list.subject.usecase.js";
+import { SubjectPgRepository } from "../persistence/subject.pg.repository.js";
+
+const router = Router();
+
+const subjectPgRepository = new SubjectPgRepository();
+const createSubjectUseCase = new CreateSubjectUseCase(subjectPgRepository);
+const listSubjectUseCase = new ListSubjectUseCase(subjectPgRepository);
+const subjectController = new SubjectController(createSubjectUseCase, listSubjectUseCase);  
+
+export const subjectRoutes = () => {
+
+router.post("/", subjectController.create);
+router.get("/", subjectController.list);
+
+}
