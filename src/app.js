8ee93ab4c58b49b7ae827b6043884da26ae4a884
Luis Benegas <luisbene12@gmail.com>
2026-04-07 20:10:46 -0300

    add router to app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { studentRoutes } from './modules/student/infrastructure/http/student.routes.js';
+import { subjectRoutes } from './modules/subjects/infrastructure/http/subject.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 
-app.use('/students', studentRoutes())
+app.use('/students', studentRoutes());
+app.use('/subjects', subjectRoutes());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
